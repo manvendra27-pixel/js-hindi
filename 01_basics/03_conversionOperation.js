2dbf2275f66f6c7c66134843ffebfbde64562781
manvendra27-pixel <manvendrasc027@gmail.com>
2025-09-07 17:17:11 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+
+// "33" -> 33 (number)
+// "33abc" -> NaN (Not a Number);
+// "Manav" -> NaN
+// undefined -> NaN 
+// null -> 0
+// true or false -> 1 or 0  but the type of these variables are same as we convertin into number 
+
+let isLoggedIn = "Manav";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(typeof isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+// 1 -> boolean : true; 0 -> boolean : false; 
+// "" -> boolean: 0 ; "Manav" -> boolean : true
+
+let sumNumber = 22;
+let isStringConversion = String(sumNumber);
+ console.log(typeof isStringConversion);
+ console.log(isStringConversion);
